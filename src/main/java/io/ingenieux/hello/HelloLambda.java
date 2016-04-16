2f0153e38a4738b40b72dff4168f1b3e9a84ad71
Aldrin Leal <aldrin@leal.eng.br>
2016-04-16 05:01:32 -0500

    Minor changes

--- src/main/java/io/ingenieux/hello/HelloLambda.java
+++ src/main/java/io/ingenieux/hello/HelloLambda.java
@@ -1,23 +1,29 @@
 package io.ingenieux.hello;
 
 import com.amazonaws.services.lambda.runtime.Context;
-
-import org.apache.commons.io.IOUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.InputStream;
 import java.io.OutputStream;
 
 import io.ingenieux.lambada.runtime.LambadaFunction;
+import io.ingenieux.lambada.runtime.LambadaUtils;
 
 public class HelloLambda {
+    private static final ObjectMapper OBJECT_MAPPER =
+            new ObjectMapper()
+                    .enable(SerializationFeature.INDENT_OUTPUT);
+
     @LambadaFunction(name="hello_sayHelloToLambda",
             description="Says Hello to Lambda",
             timeout=5)
     public void sayHelloToLambda(InputStream is, OutputStream os, Context context) throws Exception {
-        String inputPayload = IOUtils.toString(is);
+        LambadaUtils.<ObjectNode, String>wrap(OBJECT_MAPPER, is, os, ObjectNode.class, req -> {
+            System.out.println("Input Payload: " + OBJECT_MAPPER.writeValueAsString(req));
 
-        System.out.println("input payload: " + inputPayload);
-
-        IOUtils.write("\"ok\"", os);
+            return "ok";
+        });
     }
 }
